91d6788f0076f990f05d0754ee8ac9b94977290c
Peter T. Ruehr <peter.ruehr@gmail.com>
2023-07-03 18:25:11 +0200

    removed file after fusing scripts

--- Blender_Python/2_modifiers.py
+++
@@ -1,5 +0,0 @@
-import bpy
-bpy.ops.object.modifier_add(type='DECIMATE')
-bpy.context.object.modifiers["Decimate"].ratio = 0.25
-bpy.ops.object.modifier_add(type='SMOOTH')
-bpy.context.object.modifiers["Smooth"].iterations = 3
