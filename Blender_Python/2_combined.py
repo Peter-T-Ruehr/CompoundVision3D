e76a4c4b87a3205fb7fbe71755601731e03ac52c
Peter T. Ruehr <peter.ruehr@gmail.com>
2023-10-11 17:52:34 +0200

    commented out surface visualization

--- Blender_Python/2_combined.py
+++ Blender_Python/2_combined.py
@@ -5,9 +5,14 @@ bpy.ops.object.modifier_add(type='DECIMATE')
 bpy.context.object.modifiers["Decimate"].ratio = 0.25
 bpy.ops.object.modifier_add(type='SMOOTH')
 bpy.context.object.modifiers["Smooth"].iterations = 3
+bpy.context.object.modifiers["Smooth"].factor = 0.2
+
 
 # flip normales
 bpy.ops.object.editmode_toggle()
 bpy.ops.mesh.select_all(action='SELECT')
 bpy.ops.mesh.flip_normals()
 bpy.ops.object.editmode_toggle()
+
+## check normals - eye outside surface should be blue and inside red
+# bpy.context.space_data.overlay.show_face_orientation = True
