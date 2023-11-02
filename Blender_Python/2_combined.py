e1fd8a31ef83ee264fb395b2b8b5e5eee93863b5
Peter T. Ruehr <peter.ruehr@gmail.com>
2023-11-02 23:18:35 +0100

    smooth factor .3 -> .5

--- Blender_Python/2_combined.py
+++ Blender_Python/2_combined.py
@@ -5,7 +5,7 @@ bpy.ops.object.modifier_add(type='DECIMATE')
 bpy.context.object.modifiers["Decimate"].ratio = 0.25
 bpy.ops.object.modifier_add(type='SMOOTH')
 bpy.context.object.modifiers["Smooth"].iterations = 3
-bpy.context.object.modifiers["Smooth"].factor = 0.2
+bpy.context.object.modifiers["Smooth"].factor = 0.5
 
 
 # flip normales
@@ -14,5 +14,5 @@ bpy.ops.mesh.select_all(action='SELECT')
 bpy.ops.mesh.flip_normals()
 bpy.ops.object.editmode_toggle()
 
-## check normals - eye outside surface should be blue and inside red
-# bpy.context.space_data.overlay.show_face_orientation = True
+# toggle face orientation (normal) view
+#         - eye outside surface should be blue and inside red
